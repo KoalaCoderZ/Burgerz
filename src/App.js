fd16ca98c673fa26dafee6497a06ef282e86e66d
koalacoder <knightv43@gmail.com>
2018-10-26 16:48:28 +0700

    koala in act

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import NavBar from "./pages/navbar";
-import ItemCard from "./pages/card";
-import Biodata from "./forms/biodata";
-import Divider from "./pages/divider";
-=======
 import NavBar from "./components/NavBar";
 import MenuList from "./components/MenuList";
-import { connect } from 'react-redux';
-import { GET_DATA } from './actions/tableAction';
->>>>>>> 9af9f9bba1a129bb10539f80fdde232a571d1f2e
+import { connect } from "react-redux";
+import { GET_DATA } from "./actions/tableAction";
 import "./App.css";
 
 class App extends Component {
-
-  componentDidMount(){
+  componentDidMount() {
     const { GET_DATA } = this.props;
-    GET_DATA()
+    GET_DATA();
   }
 
   render() {
-    const { tableReducer: { data }} = this.props;
+    const {
+      tableReducer: { data }
+    } = this.props;
     return (
       <div className="container">
         <NavBar />
@@ -29,7 +23,7 @@ class App extends Component {
             <div
               className="col-sm-12 col-md-6 col-lg-6"
               style={{ marginBottom: 20 }}
-              key={`${item.id+1}`}
+              key={`${item.id + 1}`}
             >
               <MenuList
                 pic={item.pic}
@@ -38,10 +32,6 @@ class App extends Component {
               />
             </div>
           ))}
-        </div>
-        <Divider />
-        <div className="row">
-          <Biodata />
         </div>
       </div>
     );
@@ -52,6 +42,9 @@ const mapStateToProps = ({ tableReducer }) => ({
   tableReducer
 });
 
-export default connect(mapStateToProps,{
-  GET_DATA
-})(App);
+export default connect(
+  mapStateToProps,
+  {
+    GET_DATA
+  }
+)(App);
